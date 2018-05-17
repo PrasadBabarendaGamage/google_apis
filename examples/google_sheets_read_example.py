b8ccc272d8d7af0a9bc03fb8804240ab770de745
Prasad <psam012@aucklanduni.ac.nz>
2018-05-17 21:48:15 +1200

    Generalising read sheets example

--- examples/google_sheets_read_example.py
+++ examples/google_sheets_read_example.py
@@ -16,27 +16,45 @@ def get_sheets_service(client_secret_path='./',
                        credentials_fname='credentials.json'):
     """
     Setup the Sheets API
+    :param client_secret_path: 
+    :param client_secret_fname: 
+    :param credentials_path: 
+    :param credentials_fname: 
+    :return: 
     :rtype: Sheets API service object
     """
     store = file.Storage(os.path.join(credentials_path, credentials_fname))
     creds = store.get()
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets(
-            os.path.join(client_secret_path,client_secret_fname), SCOPES)
+            os.path.join(client_secret_path, client_secret_fname), SCOPES)
         creds = tools.run_flow(flow, store)
     return build('sheets', 'v4', http=creds.authorize(Http()))
 
+
+def read_sheets_values(spreadsheet_id, range_name):
+    """
+
+    :param SPREADSHEET_ID: 
+    :param RANGE_NAME: 
+    :return: 
+    """
+    result = read_sheets_service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id,
+        range=range_name).execute()
+    return result.get('values', [])
+
+
+# Read a sheet created by google
+spreadsheet_id = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+range_name = 'Class Data!A2:E'
+
 client_secret_path = '../'
 credentials_path = '../'
-sheets_service = get_sheets_service(
+read_sheets_service = get_sheets_service(
     credentials_path=credentials_path, client_secret_path=client_secret_path)
 # Call the Sheets API
-SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-RANGE_NAME = 'Class Data!A2:E'
-result = sheets_service.spreadsheets().values().get(
-    spreadsheetId=SPREADSHEET_ID,
-    range=RANGE_NAME).execute()
-values = result.get('values', [])
+values = read_sheets_values(spreadsheet_id, range_name)
 if not values:
     print('No data found.')
 else:
